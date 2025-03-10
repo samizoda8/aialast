865f3aed7e6cb22692223b0323dfb98180f64b60
samizoda8 <167873586+samizoda8@users.noreply.github.com>
2025-03-09 22:41:06 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -822,11 +822,3 @@ async function updateLocationInfo() {
 }
 
 updateLocationInfo();
-
-
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-document.addEventListener("keydown", (event) => {
-    if (event.ctrlKey && (event.key === "u" || event.key === "s" || event.key === "i" || event.key === "j" || event.key === "h")) {
-        event.preventDefault();
-    }
-});
